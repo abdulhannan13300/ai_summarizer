2b231bacfd6430e4a9c77e4cdf9ff68bf29d1d38
Abdul Hannan <abdul.hannan13300@gmail.com>
2023-09-05 22:49:41 +0530

    Hero and Demo Section

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -4,63 +4,143 @@ import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
-  const [article, setArticle] = useState({
-    url: "",
-    summary: "",
-  });
+   const [article, setArticle] = useState({
+      url: "",
+      summary: "",
+   });
+   const [allArticle, setAllArticle] = useState([]);
+   const [copied, setCopied] = useState(true);
 
-  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    //Fetch Summary
-    const { data } = await getSummary({ articleUrl: article.url });
+   //to not loose data when reloading or to store data in local storage
+   useEffect(() => {
+      const articlesFromLocalStorage = JSON.parse(
+         localStorage.getItem("articles")
+      );
 
-    if (data?.summary) {
-      const newArticle = { ...article, summary: data.summary };
+      if (articlesFromLocalStorage) {
+         setAllArticle(articlesFromLocalStorage);
+      }
+   }, []);
 
-      setArticle(newArticle);
-      console.log(newArticle);
-    }
-  };
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      //Fetch Summary
+      const { data } = await getSummary({ articleUrl: article.url });
 
-  return (
-    <section className="mt-16 w-full max-w-xl">
-      {/* Search */}
-      <div className="flex flex-col w-full gap-2">
-        <form
-          className="relative flex justify-center items-center"
-          onSubmit={handleSubmit}
-        >
-          <img
-            src={linkIcon}
-            alt="link_icon"
-            className="absolute left-0 my-2 ml-3 w-5"
-          />
-          <input
-            type="url"
-            placeholder="Enter a URL"
-            value={article.url}
-            onChange={(e) => setArticle({ ...article, url: e.target.value })}
-            required
-            className="url_input peer"
-          />
+      if (data?.summary) {
+         const newArticle = { ...article, summary: data.summary };
+         //add the new article to the all article array
+         const updatedArticle = [newArticle, ...allArticle];
 
-          <button
-            type="submit"
-            className="submit_btn peer-focus:border-gray-700
+         setArticle(newArticle);
+         //update the all article list
+         setAllArticle(updatedArticle);
+
+         localStorage.setItem("articles", JSON.stringify(updatedArticle));
+      }
+   };
+
+   //Copy the URL using the icon
+   const hanldeCopy = (copyURL) => {
+      setCopied(copyURL);
+      navigator.clipboard.writeText(copyURL);
+      setTimeout(() => setCopied(false), 3000);
+   };
+
+   return (
+      <section className="mt-16 w-full max-w-xl">
+         {/* Search */}
+         <div className="flex flex-col w-full gap-2">
+            <form
+               className="relative flex justify-center items-center"
+               onSubmit={handleSubmit}
+            >
+               <img
+                  src={linkIcon}
+                  alt="link_icon"
+                  className="absolute left-0 my-2 ml-3 w-5"
+               />
+               <input
+                  type="url"
+                  placeholder="Enter a URL"
+                  value={article.url}
+                  onChange={(e) =>
+                     setArticle({ ...article, url: e.target.value })
+                  }
+                  required
+                  className="url_input peer"
+               />
+
+               <button
+                  type="submit"
+                  className="submit_btn peer-focus:border-gray-700
           peer-focus:text-gray-700"
-          >
-            ↲
-          </button>
-        </form>
+               >
+                  ↲
+               </button>
+            </form>
 
-        {/* Browser URL history */}
-      </div>
+            {/* Browser URL history */}
+            <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+               {allArticle.map((item, index) => (
+                  <div
+                     key={`link-${index}`}
+                     onClick={() => setArticle(item)}
+                     className="link_card"
+                  >
+                     <div
+                        className="copy_btn"
+                        onClick={() => hanldeCopy(item.url)}
+                     >
+                        <img
+                           src={copied === item.url ? tick : copy}
+                           alt="copy_icon"
+                           className="w-[40%] h-[40%] object-contain"
+                        />
+                     </div>
+                     <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                        {item.url}
+                     </p>
+                  </div>
+               ))}
+            </div>
+         </div>
 
-      {/* Display Result */}
-    </section>
-  );
+         {/* Display Result */}
+         <div className="flex justify-center items-center my-10 max-w-full">
+            {isFetching ? (
+               <img
+                  src={loader}
+                  alt="loader"
+                  className="w-20 h-20 object-contain"
+               />
+            ) : error ? (
+               <p className="font-inter font-bold text-black text-center">
+                  Well, that wasn't supposed to happen...
+                  <span className="font-satoshi font-normal text-gray-700">
+                     {error?.data?.error}
+                  </span>
+                  <br />
+               </p>
+            ) : (
+               article.summary && (
+                  <div className="flex flex-col gap-3">
+                     <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                        Article <span className="orange_gradient">Summary</span>
+                     </h2>
+                     <div className="summary_box">
+                        <p className="font-inter font-medium text-sm text-gray-700">
+                           {article.summary}
+                        </p>
+                     </div>
+                  </div>
+               )
+            )}
+         </div>
+      </section>
+   );
 };
 
 export default Demo;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,38 @@
 import { logo } from "../assets";
 
 const Hero = () => {
-  return (
-    <header className="w-full flex justify-center items-center flex-col">
-      {/* Navbar */}
-      <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        {/* logo */}
-        <img src={logo} alt="sumz_logo" className="object-contain w-28" />
-        {/* github button */}
-        <button
-          type="button"
-          onClick={() => window.open("https://github.com/abdulhannan13300/")}
-          className="black_btn"
-        >
-          Github
-        </button>
-      </nav>
+   return (
+      <header className="w-full flex justify-center items-center flex-col">
+         {/* Navbar */}
+         <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            {/* logo */}
+            <img src={logo} alt="sumz_logo" className="object-contain w-28" />
+            {/* github button */}
+            <button
+               type="button"
+               onClick={() =>
+                  window.open(
+                     "https://github.com/abdulhannan13300/ai_summarizer"
+                  )
+               }
+               className="black_btn"
+            >
+               Github
+            </button>
+         </nav>
 
-      <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient">OpenAI GPT-4</span>
-      </h1>
+         <h1 className="head_text">
+            Summarize Articles with <br className="max-md:hidden" />
+            <span className="orange_gradient">OpenAI GPT-4</span>
+         </h1>
 
-      <h2 className="desc">
-        Simplify your reading with Summize, an open-source article summarizer
-        that transforms lengthy articles into clear and concise summaries.
-      </h2>
-    </header>
-  );
+         <h2 className="desc">
+            Simplify your reading with Summize, an open-source article
+            summarizer that transforms lengthy articles into clear and concise
+            summaries.
+         </h2>
+      </header>
+   );
 };
 
 export default Hero;
